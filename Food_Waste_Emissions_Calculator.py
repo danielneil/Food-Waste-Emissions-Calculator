1d882c227f6d19fb7fa36b83f02968e54f7dad85
Daniel Neil <danielneilhodder@gmail.com>
2021-07-23 00:16:25 +1000

    Update Food_Waste_Emissions_Calculator.py

--- Food_Waste_Emissions_Calculator.py
+++ Food_Waste_Emissions_Calculator.py
@@ -8,19 +8,21 @@ def main(argv):
    
    foodWasteKg = 0
 
+   helpString = 'Food_Waste_Emissions_Calculator.py -i <foodWasteInKG>'
+   
    try:
       opts, args = getopt.getopt(argv,"hi:",["foodWasteKg="])
       
    except getopt.GetoptError:
       
-      print 'Food_Waste_Emissions_Calculator.py -i <foodWasteInKG>'
+      print helpString
       
       sys.exit(2)
       
    for opt, arg in opts:
       
       if opt == '-h':
-         print 'Food_Waste_Emissions_Calculator.py -i <foodWasteInKG>'
+         print helpString
          sys.exit()
 
 if __name__ == "__main__":
