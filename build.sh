63e029f8b9a734c0b7ccb04c922f3e800f4627d9
Daniel Neil <danielneilhodder@gmail.com>
2021-07-23 00:36:14 +1000

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ set -e
 ##################################################
 # Now run the playbook.
 ansible-playbook ./site.yml -i hosts  
+
+# Running a smoke test of the code.
+./Food_Waste_Emissions_Calculator.py  -i 10
