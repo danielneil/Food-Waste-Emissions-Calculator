6b14ce68f45894e25b5255e9fb8e0b7b48fe2ca3
Daniel Neil <danielneilhodder@gmail.com>
2021-07-23 00:24:28 +1000

    Update Food_Waste_Emissions_Calculator.py

--- Food_Waste_Emissions_Calculator.py
+++ Food_Waste_Emissions_Calculator.py
@@ -2,11 +2,14 @@
 
 # Calculate the emissions from a specific amount of food waste entered as an argument on the command line.
 
+# For every kilo of food waste that ends up in land fill, 1.9kg of methane is released into the atmosphere.
+
 import sys, getopt
 
 def main(argv):
    
    foodWasteKg = 0
+   methaneKG = 1.9
 
    helpString = 'Food_Waste_Emissions_Calculator.py -i <foodWasteInKG>'
    
@@ -29,7 +32,8 @@ def main(argv):
       elif opt in ("-i", "--foodWasteKg"):
          foodWasteKg = arg
          
-   print foodWasteKg
+   print 'Food Waste (KG): ' + foodWasteKg + ' kg'
+   print 'Released Methane: ' + methaneKG * foodWasteKg ' kg'
          
 if __name__ == "__main__":
    
